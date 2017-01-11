ad980db4ed313f04fc1a21f4efadd4156884c58e
Morgan <morgan.bohn@unistra.fr>
2017-01-11 12:20:14 +0100

    styles

--- xml_archive_to_pdf/utils.py
+++ xml_archive_to_pdf/utils.py
@@ -8,6 +8,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import cm
+from reportlab.lib import colors
 
 
 def get_bare_tag(elem):
@@ -29,34 +30,39 @@ def get_doc(pdf_file):
 def get_title_style(level):
     """ get title style by level """
     if level == 0:
-        return "Title"
+        return "cTitle"
     elif level in [1, 2, 3, 4, 5, 6]:
-        return "{}{}".format("Heading", str(level))
+        return "{}{}".format("cHeading", str(level))
     else:
-        return "Normal"
+        return "Italic"
 
 
 def get_styles():
     """
     get styles for the pdf, default are :
-    ‘Title’: <ParagraphStyle ‘Title’>
-    ‘Heading1’: <ParagraphStyle ‘Heading1’>
-    ‘Heading2’: <ParagraphStyle ‘Heading2’>
-    ‘Heading3’: <ParagraphStyle ‘Heading3’>
-    ‘Heading4’: <ParagraphStyle ‘Heading4’>
-    ‘Heading5’: <ParagraphStyle ‘Heading5’>
-    ‘Heading6’: <ParagraphStyle ‘Heading6’>
-    ‘Bullet’: <ParagraphStyle ‘Bullet’>
-    ‘Definition’: <ParagraphStyle ‘Definition’>
-    ‘Normal’: <ParagraphStyle ‘Normal’>
-    ‘Italic’: <ParagraphStyle ‘Italic’>
-    ‘BodyText’: <ParagraphStyle ‘BodyText’>
-    ‘Code’: <ParagraphStyle ‘Code’>
+    Title: <ParagraphStyle ‘Title’>
+    Heading1: <ParagraphStyle ‘Heading1’>
+    Heading2: <ParagraphStyle ‘Heading2’>
+    Heading3: <ParagraphStyle ‘Heading3’>
+    Heading4: <ParagraphStyle ‘Heading4’>
+    Heading5: <ParagraphStyle ‘Heading5’>
+    Heading6: <ParagraphStyle ‘Heading6’>
+    Bullet: <ParagraphStyle ‘Bullet’>
+    Definition: <ParagraphStyle ‘Definition’>
+    Normal: <ParagraphStyle ‘Normal’>
+    Italic: <ParagraphStyle ‘Italic’>
+    BodyText: <ParagraphStyle ‘BodyText’>
+    Code: <ParagraphStyle ‘Code’>
     """
     styles = getSampleStyleSheet()
-    # styles.add(ParagraphStyle(name='justify', alignment=TA_JUSTIFY, leftIndent=130))
-    # styles.add(ParagraphStyle(name='right', alignment=TA_RIGHT, rightIndent=50))
-    # styles.add(ParagraphStyle(name='left', alignment=TA_LEFT, leftIndent=50))
+    styles.add(ParagraphStyle(name='cTitle', parent=styles['Title'], spaceAfter=24))
+    styles.add(ParagraphStyle(name='cHeading1', parent=styles['Heading1'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#668cff"), backColor=colors.HexColor("#668cff")))
+    styles.add(ParagraphStyle(name='cHeading2', parent=styles['Heading2'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#809fff"), backColor=colors.HexColor("#809fff")))
+    styles.add(ParagraphStyle(name='cHeading3', parent=styles['Heading3'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#99b3ff"), backColor=colors.HexColor("#99b3ff")))
+    styles.add(ParagraphStyle(name='cHeading4', parent=styles['Heading4'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#b3c6ff"), backColor=colors.HexColor("#b3c6ff")))
+    styles.add(ParagraphStyle(name='cHeading5', parent=styles['Heading5'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#ccd9ff"), backColor=colors.HexColor("#ccd9ff")))
+    styles.add(ParagraphStyle(name='cHeading6', parent=styles['Heading6'], spaceBefore=10, borderRadius=5, borderWidth=1, borderPadding=4, borderColor=colors.HexColor("#e6ecff"), backColor=colors.HexColor("#e6ecff")))
+
     return styles
 
 
@@ -77,14 +83,18 @@ def build_pdf(xml_file, pdf_file):
             value = get_clean_text(e)
             has_children = True if e.getchildren() else False
 
-            # Gestion des titres, un titre est un élément avec des enfants
+            # Gestion des titres: un titre est un élément avec des enfants
             if has_children:
-                print(label)
-                Story.append(Paragraph(label, styles[get_title_style(level)]))
-                Story.append(Spacer(1, 6))
-            # C'est un element de type clé-valeur
+                # Si on a un label non vide, on l'affiche
+                if label:
+                    Story.append(Paragraph(label, styles[get_title_style(level)]))
+                # Si on a forcé le nom du label à vide, on met juste un petit espace
+                else:
+                    Story.append(Spacer(1, 6))
+            # C'est un element de type clé-valeur, on l'affiche normalement
             else:
-                Story.append(Paragraph("{}: {}".format(label, value), styles["Normal"]))
+                if label or value:
+                    Story.append(Paragraph("{}: {}".format(label, value), styles["Normal"]))
 
         # Calcul the level
         if (action == 'start'):
