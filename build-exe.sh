df9d425de179db385a10b73b17d1022374fa810a
Morgan <morgan.bohn@unistra.fr>
2017-02-23 10:04:33 +0100

    exe for windows, v1.0.8

--- build-exe.sh
+++ build-exe.sh
@@ -8,8 +8,7 @@
 PYTHON344_MSI_NAME="python-3.4.4.msi"
 PYTHON344_MSI_URL="https://www.python.org/ftp/python/3.4.4/$PYTHON344_MSI_NAME"
 LXML_WHL_NAME="lxml-3.7.3-cp34-cp34m-win32.whl"
-LXML_REFERER='http://www.lfd.uci.edu/~gohlke/pythonlibs/'
-LXML_URL='http://www.lfd.uci.edu/~gohlke/pythonlibs/tugh5y6j/$LXML_WHL_NAME'
+LXML_URL="http://www.lfd.uci.edu/~gohlke/pythonlibs/tugh5y6j/$LXML_WHL_NAME"
 REPORTLAB_PYPI='https://www.reportlab.com/pypi/simple/'
 REPORTLAB_PKG="reportlab==3.3.34"
 DOCOPT_PKG="docopt==0.6.2"
@@ -23,12 +22,12 @@ PYTHON_FOLDER="c:\\Python3"
 PYTHON_LIBS_FOLDER="c:\\Python3\\Lib\\site-packages"
 PYTHON_SCRIPT_MAIN="xml_archive_to_pdf/main.py"
 # Exe
-EXE_NAME="xml-archive-to-pdf.exe"
+EXE_NAME="xml-archive-to-pdf-1.0.8.exe"
 EXE_DIST="dist"
 
 mkdir $TMP_FOLDER
 wget -c -O $TMP_FOLDER/$PYTHON344_MSI_NAME $PYTHON344_MSI_URL
-wget -c -O $TMP_FOLDER/$LXML_WHL_NAME --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" --referer=$LXML_REFERER $LXML_URL
+wget -c -O $TMP_FOLDER/$LXML_WHL_NAME --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" "$LXML_URL"
 WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX $WINE_CMD msiexec /i $TMP_FOLDER/$PYTHON344_MSI_NAME /qn TARGETDIR=$PYTHON_FOLDER
 WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX $WINE_CMD py -3.4 -m pip install --upgrade pip
 WINEARCH=$WINEARCH WINEPREFIX=$WINEPREFIX $WINE_CMD pip install $TMP_FOLDER/$LXML_WHL_NAME $DOCOPT_PKG pyinstaller
